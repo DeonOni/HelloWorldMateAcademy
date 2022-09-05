a062f68c038b6bef68f2b3092314241351cb329e
Maksim Trush <maksimtrush895@gmail.com>
2022-09-05 18:44:29 +0300

    change world to mates

--- HelloWorldMate/src/academy/mate/HelloWorldMain.java
+++ HelloWorldMate/src/academy/mate/HelloWorldMain.java
@@ -2,6 +2,6 @@ package academy.mate;
 
 public class HelloWorldMain {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Mates!");
     }
 }
